074bbd6562d18e1783d7e60a08befa7aa89bb728
N64N64 <>
2017-04-09 16:58:32 -0700

    server/client improvements

--- src/lua/net/client.lua
+++ src/lua/net/client.lua
@@ -1,43 +1,50 @@
-local super = require 'net.base'
+local super = Object
 Net.Client = Object.new(super)
 
-function Net.Client:new(ip, port)
+function Net.Client:new(connfd)
     local self = super.new(self)
 
-    self.ip = ip
-    self.port = port
+    self.connfd = connfd -- optional
 
     return self
 end
 
-function Net.Client:start()
-    self.connfd = C.client_start(self.ip, tostring(self.port))
-end
-
-function Net.Client:run()
-    if not self.connfd then return end
-    self.connected = C.client_is_connected(self.connfd)
-    if not self.connected then return end
-
-    local data = self:recv(self.connfd)
-    if data == false then
-        -- disconnected
-        self.connfd = nil
-    elseif data then
-        print(tostring(data))
+function Net.Client:connect(ip, port)
+    if self.connfd then
+        error('connection already established')
     end
+    self.connfd = C.client_start(ip, tostring(port))
 end
 
+function Net.Client:is_connected()
+    return self.connfd and C.client_is_connected(self.connfd)
+end
+
+function Net.Client:close()
+    if not self.connfd then return end
+
+    C.closesocket(self.connfd)
+    self.connfd = nil
+end
+
+local buf = ffi.new('char[512]')
 function Net.Client:recv()
-    if not self.connected or not self.connfd then
+    if not self:is_connected() then
         error('not connected')
     end
-    return super.recv(self, self.connfd)
+
+    local len = C.recv(self.connfd, buf, ffi.sizeof(buf), 0)
+    if len == 0 then
+        self.connfd = nil
+        return false
+    elseif len ~= -1 then
+        return ffi.string(buf, len)
+    end
 end
 
 function Net.Client:send(s)
-    if not self.connected or not self.connfd then
+    if not self:is_connected() then
         error('not connected')
     end
-    return super.send(self, self.connfd, s)
+    return C.send(self.connfd, s, #s, 0)
 end
--- src/lua/net/server.lua
+++ src/lua/net/server.lua
@@ -1,10 +1,10 @@
-local super = require 'net.base'
+local super = Object
 Net.Server = Object.new(super)
 
 function Net.Server:new(port)
     local self = super.new(self)
 
-    self.connfds = {}
+    self.clients = {}
     self.port = port
 
     local hostname = ffi.new('char[200]')
@@ -14,7 +14,7 @@ function Net.Server:new(port)
     return self
 end
 
-function Net.Server:start()
+function Net.Server:listen()
     if self.listenfd then
         error('server already started')
     end
@@ -27,11 +27,11 @@ function Net.Server:start()
     end
 end
 
-function Net.Server:stop()
-    for _,connfd in ipairs(self.connfds) do
-        C.closesocket(connfd)
+function Net.Server:close()
+    for _,client in ipairs(self.clients) do
+        client:close()
     end
-    self.connfds = {}
+    self.clients = {}
     if self.listenfd then
         C.closesocket(self.listenfd)
         self.listenfd = nil
@@ -39,7 +39,7 @@ function Net.Server:stop()
 end
 
 local function runcode(s)
-    local f, err = load(s)
+    local f, err = load(s) -- TODO sandbox this
     if not f then
         return 'err: '..err
     end
@@ -58,24 +58,24 @@ function Net.Server:run()
     local connfd = C.server_listen(self.listenfd)
     if connfd >= 0 then
         print('got connection')
-        table.insert(self.connfds, connfd)
+        table.insert(self.clients, Net.Client:new(connfd))
     end
 
     -- listen on existing connections
     local i = 0
-    while i < #self.connfds do
+    while i < #self.clients do
         i = i + 1
-        local connfd = self.connfds[i]
-        local data = self:recv(connfd)
+        local client = self.clients[i]
+        local data = client:recv()
         if data == false then
             -- disconnected
             print('disconnected')
-            table.remove(self.connfds, i)
+            table.remove(self.clients, i)
             i = i - 1
         elseif data then
             print('got some shit')
             local s = tostring(runcode(data))
-            self:send(connfd, s)
+            client:send(s)
             print(s)
         end
     end
